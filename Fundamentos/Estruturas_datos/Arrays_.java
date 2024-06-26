52687227f82a91aa4e13a670afaafdd5812793e3
luis-rios <vvrhz11@gmail.com>
2024-06-25 22:35:12 -0600

    agregando estructuras de datos

--- Fundamentos/Estruturas_datos/Arrays_.java
+++ Fundamentos/Estruturas_datos/Arrays_.java
@@ -1,16 +1,20 @@
 package Estruturas_datos;
-import java.util.Scanner;
+import java.util.*;
 
-public class Arrays {
+public class Arrays_{
   public static void recorridoArray(String arr[]){
     for (String iteracion : arr) {
       System.out.println(iteracion);
     }
   }
   public static void main(String[] args) {
-    //Declaracion del array
+    
+    //Declaracion del array.
     String Nombres [] = {"luis","yair","rios","miranda"};
     int [] Edades  = {12,16,15,29};
+    //Cambian un valor en espesifico del array.
+    Arrays.fill(Nombres,3,4,"Skoll");
+    //Llamada a la función.
     recorridoArray(Nombres);
   }
 }
